9d342498c311d3c5d6e45d3af40e278d20effd85
Prata <rodrigobp@ciandt.com>
2021-08-21 12:33:08 -0300

    section2.15

---
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/security/PasswordEncoderImpl.java
@@ -0,0 +1,12 @@
+package com.springbank.user.cmd.api.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordEncoderImpl implements PasswordEncoder {
+    @Override
+    public String hashPassword(String password) {
+       return new BCryptPasswordEncoder(12).encode(password);
+    }
+}
